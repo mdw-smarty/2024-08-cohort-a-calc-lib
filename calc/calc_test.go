85248f9df1552296da287644bf4331bc8963529d
Michael Whatcott <mike@smarty.com>
2024-08-20 11:01:07 -0600

    A few more niceties.

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -3,14 +3,16 @@ package calc
 import "testing"
 
 func TestAddition(t *testing.T) {
-	assertEqual(t, 0, Addition{}.Calculate(0, 0))
-	assertEqual(t, 1, Addition{}.Calculate(0, 1))
-	assertEqual(t, 2, Addition{}.Calculate(1, 1))
-	assertEqual(t, 7, Addition{}.Calculate(3, 4))
-	assertEqual(t, -5, Addition{}.Calculate(2, -7))
+	var calculator Addition
+	assertEqual(t, 0, calculator.Calculate(0, 0))
+	assertEqual(t, 1, calculator.Calculate(0, 1))
+	assertEqual(t, 2, calculator.Calculate(1, 1))
+	assertEqual(t, 6, calculator.Calculate(3, 4))
+	assertEqual(t, -5, calculator.Calculate(2, -7))
 }
 
 func assertEqual(t *testing.T, expected, actual any) {
+	t.Helper()
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
