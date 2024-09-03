445e6d8a7bae6a80f94fa51572ebf13313f00b93
Michael Whatcott <mike@smarty.com>
2024-09-03 10:23:48 -0600

    Extract test helpers.

--- main_test.go
+++ main_test.go
@@ -3,67 +3,62 @@ package main
 import (
 	"bytes"
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/mdwhatcott/calc-lib/calc"
 )
 
+func assertEqual(t *testing.T, expected, actual any) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Helper()
+		t.Errorf("\n"+
+			"expected: %v\n"+
+			"actual:   %v", expected, actual)
+	}
+}
+func assertError(t *testing.T, expected, actual error) {
+	if !errors.Is(actual, expected) {
+		t.Helper()
+		t.Errorf("expected [%v], got [%v]", expected, actual)
+	}
+}
+
 func TestTooFewArguments(t *testing.T) {
 	output := bytes.Buffer{}
 	handler := NewHandler(calc.Addition{}, &output)
 	err := handler.Handle(nil)
-	if !errors.Is(err, errTooFewArgs) {
-		t.Errorf("expected %v, got %v", errTooFewArgs, err)
-	}
-	if output.Len() > 0 {
-		t.Error("expected no output")
-	}
+	assertError(t, errTooFewArgs, err)
+	assertEqual(t, "", output.String())
 }
 func TestInvalidFirstArg(t *testing.T) {
 	output := bytes.Buffer{}
 	handler := NewHandler(calc.Addition{}, &output)
 	err := handler.Handle([]string{"NaN", "1"})
-	if !errors.Is(err, errMalformedArgument) {
-		t.Errorf("expected %v, got %v", errMalformedArgument, err)
-	}
-	if output.Len() > 0 {
-		t.Error("expected no output")
-	}
+	assertError(t, errMalformedArgument, err)
+	assertEqual(t, "", output.String())
 }
 func TestInvalidSecondArg(t *testing.T) {
 	output := bytes.Buffer{}
 	handler := NewHandler(calc.Addition{}, &output)
 	err := handler.Handle([]string{"1", "NaN"})
-	if !errors.Is(err, errMalformedArgument) {
-		t.Errorf("expected %v, got %v", errMalformedArgument, err)
-	}
-	if output.Len() > 0 {
-		t.Error("expected no output")
-	}
+	assertError(t, errMalformedArgument, err)
+	assertEqual(t, "", output.String())
 }
 func TestOutputWriterError(t *testing.T) {
 	taco := errors.New("taco")
 	output := &ErringWriter{err: taco}
 	handler := NewHandler(calc.Addition{}, output)
 	err := handler.Handle([]string{"1", "2"})
-	if !errors.Is(err, errOutputWriteErr) {
-		t.Errorf("expected %v, got %v", errOutputWriteErr, err)
-	}
-	if !errors.Is(err, taco) {
-		t.Errorf("expected %v, got %v", taco, err)
-	}
+	assertError(t, errOutputWriteErr, err)
+	assertError(t, taco, err)
 }
 func TestHappyPath(t *testing.T) {
 	output := bytes.Buffer{}
 	handler := NewHandler(calc.Addition{}, &output)
 	err := handler.Handle([]string{"1", "2"})
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
-	}
-	actual := output.String()
-	if actual != "3\n" {
-		t.Errorf("expected %s, got %s", "3", actual)
-	}
+	assertEqual(t, nil, err)
+	assertEqual(t, "3\n", output.String())
 }
 
 type ErringWriter struct {
