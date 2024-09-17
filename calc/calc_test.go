0882a8a5060976baab474fb897281bd50db2587a
Michael Whatcott <mike@smarty.com>
2024-09-17 10:24:28 -0600

    Subtraction, Division, Multiplication

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -10,6 +10,28 @@ func TestAddition(t *testing.T) {
 	assertEqual(t, 6, calculator.Calculate(3, 4))
 	assertEqual(t, -5, calculator.Calculate(2, -7))
 }
+func TestSubtraction(t *testing.T) {
+	var calculator Subtraction
+	assertEqual(t, 0, calculator.Calculate(0, 0))
+	assertEqual(t, -1, calculator.Calculate(0, 1))
+	assertEqual(t, 0, calculator.Calculate(1, 1))
+	assertEqual(t, -1, calculator.Calculate(3, 4))
+	assertEqual(t, 9, calculator.Calculate(2, -7))
+}
+func TestMultiplication(t *testing.T) {
+	var calculator Multiplication
+	assertEqual(t, 0, calculator.Calculate(0, 0))
+	assertEqual(t, 0, calculator.Calculate(0, 1))
+	assertEqual(t, 1, calculator.Calculate(1, 1))
+	assertEqual(t, 12, calculator.Calculate(3, 4))
+	assertEqual(t, -14, calculator.Calculate(2, -7))
+}
+func TestDivision(t *testing.T) {
+	var calculator Division
+	assertEqual(t, 0, calculator.Calculate(0, 1))
+	assertEqual(t, 1, calculator.Calculate(1, 1))
+	assertEqual(t, 3, calculator.Calculate(12, 4))
+}
 
 func assertEqual(t *testing.T, expected, actual any) {
 	t.Helper()
